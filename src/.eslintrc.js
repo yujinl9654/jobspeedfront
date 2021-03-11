9e841ca6228466a47e4aa4da8bdf90590a2340f3
enoch1118 <enoch4921@gmail.com>
2021-03-11 18:05:53 +0900

    lint

---
+++ src/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'naver',
+    'plugin:prettier/recommended',
+  ],
+  // "parserOptions": {
+  //     "ecmaFeatures": {
+  //         "jsx": true
+  //     },
+  //     "ecmaVersion": 12,
+  //     "sourceType": "module"
+  // },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    'prefer-template': 'off',
+    'class-methods-use-this': 'off',
+    'consistent-this': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+  },
+};
